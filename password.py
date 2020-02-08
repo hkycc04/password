6b201ea5e11b367daefb745b3f05109b1a753df7
Alan Huang <hkycc04@gmail.com>
2020-02-08 20:37:14 +0800

    second version

--- password.py
+++ password.py
@@ -1,8 +1,12 @@
-#passw = 'a123456'
+password = 'a123456'
+i = 3
 while True:
-	password = input('請輸入您的密碼: ')
-	if password == 'a123456':
+	pwd = input('請輸入密碼')
+	if pwd == password:
 		print('登入成功')
 		break
-	elif password != 'a123456':
-		print('還有二次機會')
+	else:
+		i = i - 1
+		print('密碼錯誤! 還有', i, '次機會')
+		if i == 0:
+			break
